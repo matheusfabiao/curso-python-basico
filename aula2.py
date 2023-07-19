65d74720984e55a0585f1750cf5636845d6d0dec
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-07-18 21:13:12 -0300

    aula 1

---
+++ aula2.py
@@ -0,0 +1,13 @@
+nome = "Matheus"
+idade = 27
+altura = 1.78
+
+print(nome)
+print(idade)
+print(altura)
+print("-"*40)
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print("-"*40)
+print(f"{nome} tem {idade} anos e {altura} de altura")
