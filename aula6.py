b5cbe58bd2f2221c9d0b678ea0d31e9ce27bc437
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-09 16:14:38 -0300

    aula 6 concluída

---
+++ aula6.py
@@ -0,0 +1,14 @@
+minha_lista = ['Gui', 'João']  # Lista (list)
+minha_tupla = ('Gui', 'João')  # Tupla (tuple)
+meu_conjunto = {'Gui', 'João'}  # Conjunto (set)
+meu_dicionario = {'nome': 'Guilherme', 'idade': 27}  # Dicionario (dict)
+
+minha_lista.append('Matheus')
+print(minha_lista)
+minha_lista.pop()
+print(minha_lista)
+minha_lista.clear()
+print(minha_lista)
+
+meu_conjunto.add('Matheus')
+meu_conjunto.remove('Matheus')
